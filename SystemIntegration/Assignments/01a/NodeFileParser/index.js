acd4d86cfb537a2334e1a509a2003eef7eac7f34
Rex77pro <smaurieka@gmail.com>
2025-02-05 12:28:47 +0100

    Feat(XmlParse): Print xml to one string

--- SystemIntegration/Assignments/01a/NodeFileParser/index.js
+++ SystemIntegration/Assignments/01a/NodeFileParser/index.js
@@ -6,7 +6,20 @@ const XmlParser = new Parser(filePath + '.xml');
 
 XmlParser.XmlFileParser()
     .then(data => {
-        console.log('Parsed XMl ', JSON.stringify(data, null, 2));
+
+        const pokemon = data.pokemon;
+        const name = pokemon.name[0];
+        const nationalNo = pokemon.nationalNo[0];
+        const types = pokemon.types[0].type.join(", ");
+        const resultString = `Pokemon: ${name} (#${nationalNo}), Types: ${types}`;
+        
+        console.log()
+        console.log("Xml File: ");
+        console.log(resultString);
+        console.log()
+
+        //console.log('Parsed XMl ', JSON.stringify(data, null, 2));
+        
     })
     .catch(error =>{
         console.error('Fejl: ', error)
